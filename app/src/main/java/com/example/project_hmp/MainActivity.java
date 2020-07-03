c443f81588f313a3b4560ec84ccd939d8c7bd39a
Hur Kyung Doo <1kdhur2@naver.com>
2020-07-03 16:13:49 +0900

    대답하라고!!!!!!!!!!!!!!!!!!!!

--- app/src/main/java/com/example/project_hmp/MainActivity.java
+++ app/src/main/java/com/example/project_hmp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //이의재바부똥개
+    //대답하라고!!!!!!!!!!!!!!!!!!!!!!!!!!!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
