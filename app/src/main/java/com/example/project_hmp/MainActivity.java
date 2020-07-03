39d24773b84e87f6abddf2770f4e382cdb8f3104
Hur Kyung Doo <1kdhur2@naver.com>
2020-07-03 13:38:53 +0900

    test

--- app/src/main/java/com/example/project_hmp/MainActivity.java
+++ app/src/main/java/com/example/project_hmp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //이의재바부
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
